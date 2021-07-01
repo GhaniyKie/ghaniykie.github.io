18a9e867820105ffaf019674e235ebe5c66a347c
= <abdulhakimghaniy@outlook.co.id>
2021-07-01 11:10:14 +0700

    Perubahan dan Implementasi Mobile Responsive

---
+++ assets/javascripts/custom.js
@@ -0,0 +1,32 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Password Visibility Toggle
+const password = document.querySelector("#formPassword");
+const togglePassword = document.querySelector("#togglePassword");
+
+const passwordConfirm = document.querySelector("#formPasswordConfirm");
+const togglePasswordConfirm = document.querySelector("#togglePasswordConfirm");
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
+
+togglePasswordConfirm.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = passwordConfirm.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordConfirm.setAttribute('type', type);
+    // toggle the eye / eye slash icon
+    this.classList.toggle('bi-eye');
+});
